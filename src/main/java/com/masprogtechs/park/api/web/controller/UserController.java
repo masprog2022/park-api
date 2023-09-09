4476960ea6dae44cf070adf1829bdcf8fe2a4adb
masprog2022 <masprog2022@gmail.com>
2023-09-09 13:48:49 +0100

    :sparkles: criando UserController

--- src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
@@ -1,2 +1,14 @@
-package com.masprogtechs.park.api.web.controller;public class UserController {
+package com.masprogtechs.park.api.web.controller;
+
+import com.masprogtechs.park.api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    private final UserService userService;
 }
