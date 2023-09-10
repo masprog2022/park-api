c9db62306c13f761d6337ddca8012faaa4551012
masprog2022 <masprog2022@gmail.com>
2023-09-10 15:16:31 +0100

    :sparkles: criando metódo getById no UserController

--- src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
@@ -5,10 +5,7 @@ import com.masprogtechs.park.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +18,11 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user){
         User userSave = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
