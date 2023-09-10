c9e7e46ed8b07c409d8a102e680e24dffd9c99da
masprog2022 <masprog2022@gmail.com>
2023-09-10 15:17:20 +0100

    :sparkles: criando o metodo findById em UserService

--- src/main/java/com/masprogtechs/park/api/service/UserService.java
+++ src/main/java/com/masprogtechs/park/api/service/UserService.java
@@ -16,4 +16,11 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+    }
 }
