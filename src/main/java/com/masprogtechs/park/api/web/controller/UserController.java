9a3334f6ffab168df1797d33f9ef51d1e3425970
masprog2022 <masprog2022@gmail.com>
2023-09-10 14:26:55 +0100

    :sparkles: criando metódo create na controller

--- src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.masprogtechs.park.api.web.controller;
 
+import com.masprogtechs.park.api.entity.User;
 import com.masprogtechs.park.api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userSave = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
+    }
 }
