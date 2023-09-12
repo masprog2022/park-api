6dead93d88aa1862d868d5ab9554d417c5e549c0
masprog2022 <masprog2022@gmail.com>
2023-09-12 02:39:12 +0100

    :sparkles: feat: adicionando o metodo toListDto em UserMapper

--- src/main/java/com/masprogtechs/park/api/web/dto/mapper/UserMapper.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/mapper/UserMapper.java
@@ -6,6 +6,9 @@ import com.masprogtechs.park.api.web.dto.UserResponseDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserMapper {
     public static User toUser(UserCreateDto createDto){
         return new ModelMapper().map(createDto, User.class);
@@ -23,5 +26,9 @@ public class UserMapper {
         mapper.addMappings(props);
 
         return mapper.map(user, UserResponseDto.class);
+    }
+
+    public static List<UserResponseDto> toListDto(List<User> users){
+        return users.stream().map(user -> toDto(user)).collect(Collectors.toList());
     }
 }
