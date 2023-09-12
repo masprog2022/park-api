81f812ebd14cb7a765fff8a45326553152cd87f4
masprog2022 <masprog2022@gmail.com>
2023-09-12 12:32:09 +0100

    :sparkles: feat: adicionando jakarta bean validation no dto UserCreateDto

--- src/main/java/com/masprogtechs/park/api/web/dto/UserCreateDto.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/UserCreateDto.java
@@ -1,5 +1,8 @@
 package com.masprogtechs.park.api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,11 @@ import lombok.*;
 @ToString
 public class UserCreateDto {
 
+    @NotBlank
+    @Email(message = "formato do email é inválido", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
