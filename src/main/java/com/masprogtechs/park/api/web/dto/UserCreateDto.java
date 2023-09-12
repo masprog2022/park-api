eb90ae85aac61473a4313fa7d9178c1affd2f6a1
masprog2022 <masprog2022@gmail.com>
2023-09-12 01:52:13 +0100

    :sparkles: criando UserCreateDto

--- src/main/java/com/masprogtechs/park/api/web/dto/UserCreateDto.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/UserCreateDto.java
@@ -1,2 +1,14 @@
-package com.masprogtechs.park.api.web.dto;public class UserCreateDto {
+package com.masprogtechs.park.api.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserCreateDto {
+
+    private String username;
+    private String password;
 }
