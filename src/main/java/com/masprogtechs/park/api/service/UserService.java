1fd8607785d6466a31edab3852105c749462d956
masprog2022 <masprog2022@gmail.com>
2023-09-12 02:21:43 +0100

    :sparkles: feat: adicionando dto no metodo updatePasswrod no UserService

--- src/main/java/com/masprogtechs/park/api/service/UserService.java
+++ src/main/java/com/masprogtechs/park/api/service/UserService.java
@@ -27,9 +27,18 @@ public class UserService {
     }
 
     @Transactional
-    public User updatePassword(Long id, String password) {
+    public User updatePassword(Long id, String currentPassword, String newPassword, String confirmPassword) {
+
+        if(!newPassword.equals(confirmPassword)){
+            throw new RuntimeException("Nova senha não confere com a confirmação de senha.");
+        }
+
         User user = findById(id);
-        user.setPassword(password);
+        if(!user.getPassword().equals(currentPassword)){
+            throw new RuntimeException("Sua senha não confere.");
+        }
+
+        user.setPassword(newPassword);
 
         return user;
     }
