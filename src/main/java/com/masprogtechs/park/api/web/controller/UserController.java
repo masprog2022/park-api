4aa38ad76ff8dfc8b9cec0d9ac492d38b1204dac
masprog2022 <masprog2022@gmail.com>
2023-09-12 01:53:14 +0100

    :sparkles: fest: alterando o metodo create para retornar e enviar dados apartir de dtos

--- src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
@@ -2,10 +2,15 @@ package com.masprogtechs.park.api.web.controller;
 
 import com.masprogtechs.park.api.entity.User;
 import com.masprogtechs.park.api.service.UserService;
+import com.masprogtechs.park.api.web.dto.UserCreateDto;
+import com.masprogtechs.park.api.web.dto.UserResponseDto;
+import com.masprogtechs.park.api.web.dto.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,16 +19,28 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+  /*  @PostMapping
     public ResponseEntity<User> create(@RequestBody User user){
         User userSave = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
+    }*/
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@RequestBody UserCreateDto createDto){
+        User user = userService.save(UserMapper.toUser(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok(users);
     }
 
     @PatchMapping("/{id}")
