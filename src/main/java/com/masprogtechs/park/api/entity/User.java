38431bfb8660bc1240cef6e5bbb2b7c70e71f4de
masprog2022 <masprog2022@gmail.com>
2023-09-22 18:12:32 +0100

    :sparkles: feat: inicializar role com valor default ROLE_ADMIN

--- src/main/java/com/masprogtechs/park/api/entity/User.java
+++ src/main/java/com/masprogtechs/park/api/entity/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 25)
-    private Role role = Role.ROLE_CUSTOMER;
+    private Role role = Role.ROLE_ADMIN;
 
     @Column(name = "created_At")
     private LocalDateTime createdAt;
