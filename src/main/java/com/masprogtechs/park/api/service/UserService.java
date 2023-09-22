00dac761fc725bcd9b8b5c1f95355fa58b31937c
masprog2022 <masprog2022@gmail.com>
2023-09-22 11:21:38 +0100

    :sparkles: feat: usando PasswordEncoder para codificar a passe

--- src/main/java/com/masprogtechs/park/api/service/UserService.java
+++ src/main/java/com/masprogtechs/park/api/service/UserService.java
@@ -8,6 +8,7 @@ import com.masprogtechs.park.api.exception.UsernameUniqueViolationException;
 import com.masprogtechs.park.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +19,12 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public User save(User user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return userRepository.save(user);
         } catch (DataIntegrityViolationException ex){
             throw new UsernameUniqueViolationException(String.format("Username %s já cadastrado", user.getUsername()));
@@ -44,11 +47,11 @@ public class UserService {
         }
 
         User user = findById(id);
-        if(!user.getPassword().equals(currentPassword)){
+        if(!passwordEncoder.matches(currentPassword, user.getPassword())){
             throw new PasswordInvalidException("Sua senha não confere.");
         }
 
-        user.setPassword(newPassword);
+        user.setPassword(passwordEncoder.encode(newPassword));
 
         return user;
     }
