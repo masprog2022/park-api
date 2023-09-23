e8cf67404b2b6eb1db993790365832f05ff68e92
masprog2022 <masprog2022@gmail.com>
2023-09-23 13:16:22 +0100

    :sparkles: feat: permissão de accesso para o ADMIN acessar os user pelo id

--- src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/UserController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -58,6 +59,7 @@ public class UserController {
     }*/
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Recuperar um usuário pelo id", description = "Recuperar um usuário pelo id",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
