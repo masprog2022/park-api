7301ee8b627455fc61a94e5ea2abd844fe9239b8
masprog2022 <masprog2022@gmail.com>
2023-09-25 13:24:43 +0100

    :sparkles: feat: adicionando customer Mapper

--- src/main/java/com/masprogtechs/park/api/web/dto/mapper/CustomerMapper.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/mapper/CustomerMapper.java
@@ -1,2 +1,20 @@
-package com.masprogtechs.park.api.web.dto.mapper;public class CustomerMapper {
+package com.masprogtechs.park.api.web.dto.mapper;
+
+import com.masprogtechs.park.api.entity.Customer;
+import com.masprogtechs.park.api.web.dto.CustomerCreateDto;
+import com.masprogtechs.park.api.web.dto.CustomerResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CustomerMapper {
+
+    public static Customer toCustomer(CustomerCreateDto dto){
+      return new ModelMapper().map(dto, Customer.class);
+    }
+
+    public static CustomerResponseDto toDto(Customer customer){
+        return new ModelMapper().map(customer, CustomerResponseDto.class);
+    }
 }
