931e55ff65d3adc1e8e6a8cf92e3f8db50c85930
masprog2022 <masprog2022@gmail.com>
2023-09-25 13:58:06 +0100

    :sparkles: feat: adicionando customer service, metodo save

--- src/main/java/com/masprogtechs/park/api/service/CustomerService.java
+++ src/main/java/com/masprogtechs/park/api/service/CustomerService.java
@@ -1,2 +1,28 @@
-package com.masprogtechs.park.api.service;public class CustomerService {
+package com.masprogtechs.park.api.service;
+
+import com.masprogtechs.park.api.entity.Customer;
+import com.masprogtechs.park.api.exception.CpfUniqueViolationException;
+import com.masprogtechs.park.api.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Transactional
+    public Customer save(Customer customer){
+        try{
+
+            return customerRepository.save(customer);
+
+        }catch (DataIntegrityViolationException ex){
+          throw  new CpfUniqueViolationException(String.format("CPF %s não pode ser cadastrado, já existe no sistema", customer.getCpf()));
+        }
+    }
+
 }
