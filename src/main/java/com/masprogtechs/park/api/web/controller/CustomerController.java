b793e0209884a779970b31c3524c46725295362c
masprog2022 <masprog2022@gmail.com>
2023-09-28 23:24:24 +0100

    :sparkles: feat: documentar para recurso de listagem de clientes

--- src/main/java/com/masprogtechs/park/api/web/controller/CustomerController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/CustomerController.java
@@ -14,6 +14,8 @@ import com.masprogtechs.park.api.web.dto.mapper.CustomerMapper;
 import com.masprogtechs.park.api.web.dto.mapper.PageableMapper;
 import com.masprogtechs.park.api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,10 +25,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -84,9 +87,36 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerMapper.toDto(customer));
     }
 
+    @Operation(summary = "Recuperar lista de clientes",
+            description = "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN' ",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {
+                    @Parameter(in = QUERY, name = "page",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "0")),
+                            description = "Representa a página retornada"
+                    ),
+                    @Parameter(in = QUERY, name = "size",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "5")),
+                            description = "Representa o total de elementos por página"
+                    ),
+                    @Parameter(in = QUERY, name = "sort", hidden = true,
+                            array = @ArraySchema(schema = @Schema(type = "string", defaultValue = "nome,asc")),
+                            description = "Representa a ordenação dos resultados. Aceita multiplos critérios de ordenação são suportados.")
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = CustomerResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            })
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PageableDto> getAll(Pageable pageable){
+    public ResponseEntity<PageableDto> getAll(@Parameter(hidden = true)
+                                                  @PageableDefault(size = 5, sort = {"name"})Pageable pageable){
         Page<CustomerProjection> customers = customerService.findAll(pageable);
         return ResponseEntity.ok(PageableMapper.toDto(customers));
     }
