e0df88fcd313d964370f2a8f754dcc07a40822e1
masprog2022 <masprog2022@gmail.com>
2023-09-28 12:03:26 +0100

    :sparkles: feat: getAll no controller customer

--- src/main/java/com/masprogtechs/park/api/web/controller/CustomerController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/CustomerController.java
@@ -18,11 +18,15 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -75,5 +79,12 @@ public class CustomerController {
     public ResponseEntity<CustomerResponseDto> getById(@PathVariable Long id){
         Customer customer = customerService.findById(id);
         return ResponseEntity.ok(CustomerMapper.toDto(customer));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<Customer>> getAll(Pageable pageable){
+        Page<Customer> customers = customerService.findAll(pageable);
+        return ResponseEntity.ok(customers);
     }
 }
