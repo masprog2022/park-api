29ed5eed121fbb78ede68850ebd83031561c5963
masprog2022 <masprog2022@gmail.com>
2023-09-28 09:22:27 +0100

    :test_tube: test: testes end to end para create customer status 201

--- src/test/java/com/masprogtechs/park/api/CustomerIT.java
+++ src/test/java/com/masprogtechs/park/api/CustomerIT.java
@@ -1,2 +1,38 @@
-package com.masprogtechs.park.api;public class CustomerIT {
+package com.masprogtechs.park.api;
+
+import com.masprogtechs.park.api.web.dto.CustomerCreateDto;
+import com.masprogtechs.park.api.web.dto.CustomerResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/customers/customers-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/customers/customers-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class CustomerIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void createCustomer_WithDateValid_ReturnCustomerWithStatus201(){
+       CustomerResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@gmail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Tobias Ferreira", "76327474004"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(CustomerResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getName()).isEqualTo("Tobias Ferreira");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("76327474004");
+    }
 }
