539ec0e198d6390f6a4efc4b8149fd1312046334
masprog2022 <masprog2022@gmail.com>
2023-09-29 01:02:17 +0100

    :sparkles: feat: adicionando SlotRepository

--- src/main/java/com/masprogtechs/park/api/repository/SlotRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/SlotRepository.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.park.api.repository;public interface SlotRepository {
+package com.masprogtechs.park.api.repository;
+
+import com.masprogtechs.park.api.entity.Slot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SlotRepository extends JpaRepository<Slot, Long> {
 }
