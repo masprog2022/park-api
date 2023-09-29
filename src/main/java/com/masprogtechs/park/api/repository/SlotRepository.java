40a094e63484fab6b53b225089d1982793bc398c
masprog2022 <masprog2022@gmail.com>
2023-09-29 11:00:23 +0100

    :sparkles: feat: adicionando metodo findByCode

--- src/main/java/com/masprogtechs/park/api/repository/SlotRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/SlotRepository.java
@@ -3,5 +3,8 @@ package com.masprogtechs.park.api.repository;
 import com.masprogtechs.park.api.entity.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+    Optional<Slot> findByCode(String code);
 }
