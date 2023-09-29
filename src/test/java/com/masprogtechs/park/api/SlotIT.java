3f550be81b0c3288ab8c24c7a4dbe318cef1f873
masprog2022 <masprog2022@gmail.com>
2023-09-30 00:09:23 +0100

    :test_tube: test: testando recurso para inserir vagas status 201

--- src/test/java/com/masprogtechs/park/api/SlotIT.java
+++ src/test/java/com/masprogtechs/park/api/SlotIT.java
@@ -1,2 +1,34 @@
-package com.masprogtechs.park.api;public class SlotIT {
+package com.masprogtechs.park.api;
+
+
+import com.masprogtechs.park.api.web.dto.SlotCreateDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/slots/slots-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/slots/slots-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class SlotIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void createSlot_WithDataValid_ReturnLocationStatus201(){
+           testClient
+                .post()
+                .uri("/api/v1/slots")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@gmail.com", "123456"))
+               .bodyValue(new SlotCreateDto("A-05", "FREE"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists(HttpHeaders.LOCATION);
+
+    }
 }
