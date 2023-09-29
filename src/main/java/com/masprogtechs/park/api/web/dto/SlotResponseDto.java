dd9a6472d914165776aeab9a95bf8b0661c262ca
masprog2022 <masprog2022@gmail.com>
2023-09-29 01:09:18 +0100

    :sparkles: feat: adicionando dto SlotResponseDto

--- src/main/java/com/masprogtechs/park/api/web/dto/SlotResponseDto.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/SlotResponseDto.java
@@ -1,2 +1,15 @@
-package com.masprogtechs.park.api.web.dto;public class SlotResponseDto {
+package com.masprogtechs.park.api.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SlotResponseDto {
+
+    private Long id;
+    private String code;
+    private  String status;
+
 }
