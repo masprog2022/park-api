8c5463882829dc749728cf1a8c7471a1cb0c3d9f
masprog2022 <masprog2022@gmail.com>
2023-09-30 01:39:42 +0100

    :sparkles: feat: adicionando ParkResponseDto

--- src/main/java/com/masprogtechs/park/api/web/dto/ParkResponseDto.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/ParkResponseDto.java
@@ -1,2 +1,30 @@
-package com.masprogtechs.park.api.web.dto;public class ParkResponseDto {
+package com.masprogtechs.park.api.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkResponseDto {
+
+    private String plate;
+    private String make;
+    private String model;
+    private String color;
+    private String customerCpf;
+    private String receipt;
+    private LocalDateTime inputData;
+    private LocalDateTime outputData;
+    private String slotCode;
+    private BigDecimal value;
+    private BigDecimal discount;
 }
