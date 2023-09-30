e6fd5d31a9d8e960e3fb27ae394143a90a918a6c
masprog2022 <masprog2022@gmail.com>
2023-09-30 02:32:02 +0100

    :sparkles: feat: add metodo findByCpf no repository

--- src/main/java/com/masprogtechs/park/api/repository/CustomerRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/CustomerRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query("select c from Customer c")
     Page<CustomerProjection> findAllPageable(Pageable pageable);
 
 
     Customer findByUserId(Long id);
+
+    Optional<Customer> findByCpf(String cpf);
 }
