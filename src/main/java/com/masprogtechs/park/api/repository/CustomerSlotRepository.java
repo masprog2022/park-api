300ca952a500ff7b0b691b440f2d50371cc0e25a
masprog2022 <masprog2022@gmail.com>
2023-09-30 02:06:56 +0100

    :sparkles: feat: add CustomerSlotRepository

--- src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.park.api.repository;public class CustomerSlotRepository {
+package com.masprogtechs.park.api.repository;
+
+import com.masprogtechs.park.api.entity.CustomerSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerSlotRepository extends JpaRepository<CustomerSlot, Long> {
 }
