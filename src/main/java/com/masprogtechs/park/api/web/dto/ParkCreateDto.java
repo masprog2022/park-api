5dcc6b9ed90163c2d2d008d7651fc5437b4b7b8b
masprog2022 <masprog2022@gmail.com>
2023-09-30 01:40:01 +0100

    :sparkles: feat: adicionando ParkCreateDto

--- src/main/java/com/masprogtechs/park/api/web/dto/ParkCreateDto.java
+++ src/main/java/com/masprogtechs/park/api/web/dto/ParkCreateDto.java
@@ -1,4 +1,34 @@
 package com.masprogtechs.park.api.web.dto;
 
-public class ParkDto {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkCreateDto {
+
+    @NotBlank
+    @Size(min = 8, max = 8)
+    @Pattern(regexp = "[A-Z]{3}-[0-9]{4}", message = "A placa do veículo deve seguir o padrão 'XXX-000'")
+    private String plate;
+
+    @NotBlank
+    private String make;
+
+    @NotBlank
+    private String model;
+
+    @NotBlank
+    private String color;
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    @CPF
+    private String customerCpf;
+
 }
