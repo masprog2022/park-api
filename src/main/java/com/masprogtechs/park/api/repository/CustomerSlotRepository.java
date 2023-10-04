ed323b7d9a138e58cf8dd65399ebda727c5bf226
masprog2022 <masprog2022@gmail.com>
2023-10-04 23:05:15 +0100

    :sparkles: feat: adicionando metodo findByReceiptAndOutputDataIsNull no repository

--- src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
@@ -3,5 +3,8 @@ package com.masprogtechs.park.api.repository;
 import com.masprogtechs.park.api.entity.CustomerSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerSlotRepository extends JpaRepository<CustomerSlot, Long> {
+    Optional<CustomerSlot> findByReceiptAndOutputDataIsNull(String receipt);
 }
