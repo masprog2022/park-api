e121c493c5e307847189af0d9f0562a5a78acaf8
masprog2022 <masprog2022@gmail.com>
2023-10-04 23:04:05 +0100

    :sparkles: feat: adicionando metodo findByReceipt no service

--- src/main/java/com/masprogtechs/park/api/service/CustomerSlotService.java
+++ src/main/java/com/masprogtechs/park/api/service/CustomerSlotService.java
@@ -1,6 +1,7 @@
 package com.masprogtechs.park.api.service;
 
 import com.masprogtechs.park.api.entity.CustomerSlot;
+import com.masprogtechs.park.api.exception.EntityRuntimeException;
 import com.masprogtechs.park.api.repository.CustomerSlotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,14 @@ public class CustomerSlotService {
     @Transactional
     public CustomerSlot save(CustomerSlot customerSlot){
         return repository.save(customerSlot);
+    }
+
+    @Transactional(readOnly = true)
+    public CustomerSlot findByReceipt(String receipt) {
+        return repository.findByReceiptAndOutputDataIsNull(receipt).orElseThrow(
+                () -> new EntityRuntimeException(String.format(
+                        "Recibo %s não encontrado no sistema ou check-out já realizado", receipt
+                ))
+        );
     }
 }
