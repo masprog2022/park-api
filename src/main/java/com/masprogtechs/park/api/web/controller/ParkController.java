70a06540e886391be610c59eea2bfdef09d0802a
masprog2022 <masprog2022@gmail.com>
2023-10-05 16:36:35 +0100

    :sparkles: feat: adicionando metodo getAllParkByCpf na controller

--- src/main/java/com/masprogtechs/park/api/web/controller/ParkController.java
+++ src/main/java/com/masprogtechs/park/api/web/controller/ParkController.java
@@ -1,15 +1,19 @@
 package com.masprogtechs.park.api.web.controller;
 
 import com.masprogtechs.park.api.entity.CustomerSlot;
+import com.masprogtechs.park.api.repository.projection.CustomerSlotProjection;
 import com.masprogtechs.park.api.service.CustomerSlotService;
 import com.masprogtechs.park.api.service.ParkService;
+import com.masprogtechs.park.api.web.dto.PageableDto;
 import com.masprogtechs.park.api.web.dto.ParkCreateDto;
 import com.masprogtechs.park.api.web.dto.ParkResponseDto;
 import com.masprogtechs.park.api.web.dto.mapper.CustomerSlotMapper;
+import com.masprogtechs.park.api.web.dto.mapper.PageableMapper;
 import com.masprogtechs.park.api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,6 +21,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,6 +34,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 
 @Tag(name = "Estacionamentos", description = "Operações de registro de entrada e saída de um veículo do estacionamento.")
 @RequiredArgsConstructor
@@ -116,6 +125,45 @@ public class ParkController {
         CustomerSlot customerSlot = parkService.checkout(receipt);
         ParkResponseDto dto = CustomerSlotMapper.toDto(customerSlot);
         return ResponseEntity.ok(dto);
+    }
+
+
+    @Operation(summary = "Localizar os registros de estacionamentos do cliente por CPF", description = "Localizar os " +
+            "registros de estacionamentos do cliente por CPF. Requisição exige uso de um bearer token.",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {
+                    @Parameter(in = PATH, name = "cpf", description = "Nº do CPF referente ao cliente a ser consultado",
+                            required = true
+                    ),
+                    @Parameter(in = QUERY, name = "page", description = "Representa a página retornada",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "0"))
+                    ),
+                    @Parameter(in = QUERY, name = "size", description = "Representa o total de elementos por página",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "5"))
+                    ),
+                    @Parameter(in = QUERY, name = "sort", description = "Campo padrão de ordenação 'inputData,asc'. ",
+                            array = @ArraySchema(schema = @Schema(type = "string", defaultValue = "inputData,asc")),
+                            hidden = true
+                    )
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = PageableDto.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
+    @GetMapping("/cpf/{cpf}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PageableDto> getAllParkByCpf(@PathVariable String cpf,
+                                                       @PageableDefault(size = 5, sort = "inputData",
+                                                       direction = Sort.Direction.ASC) Pageable pageable ){
+
+        Page<CustomerSlotProjection> projection = customerSlotService.findAllByCustomerCpf(cpf, pageable);
+        PageableDto dto = PageableMapper.toDto(projection);
+        return ResponseEntity.ok(dto);
+
     }
 
 }
