570f6d12cd6dec368a20dbac3abc8c471c51c70f
masprog2022 <masprog2022@gmail.com>
2023-10-05 16:35:06 +0100

    :sparkles: feat: adicionando metodo findAllByCustomerCpf

--- src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
+++ src/main/java/com/masprogtechs/park/api/repository/CustomerSlotRepository.java
@@ -1,6 +1,9 @@
 package com.masprogtechs.park.api.repository;
 
 import com.masprogtechs.park.api.entity.CustomerSlot;
+import com.masprogtechs.park.api.repository.projection.CustomerSlotProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +12,6 @@ public interface CustomerSlotRepository extends JpaRepository<CustomerSlot, Long
     Optional<CustomerSlot> findByReceiptAndOutputDataIsNull(String receipt);
 
     long countByCustomerCpfAndOutputDataIsNotNull(String cpf);
+
+    Page<CustomerSlotProjection> findAllByCustomerCpf(String cpf, Pageable pageable);
 }
