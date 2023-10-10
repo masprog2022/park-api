aad92df56269f30e71efc0aa01809c8add7b0dd5
masprog2022 <masprog2022@gmail.com>
2023-10-10 12:14:29 +0100

    sparkles feat: alteração do metódo para calcular o custo do parque

--- src/main/java/com/masprogtechs/park/api/util/ParkUtils.java
+++ src/main/java/com/masprogtechs/park/api/util/ParkUtils.java
@@ -12,31 +12,26 @@ import java.time.temporal.ChronoUnit;
 public class ParkUtils {
 
 
-    private static final double FIRST_15_MINUTES = 0.00;
+    private static final double FIRST_0_MINUTES = 0.00;
     private static final double FIRST_60_MINUTES = 300.00;
 
     private static final double ADDITIONAL_15_MINUTES = 1.75;
     private static final double PERCENTAGE_DISCOUNT = 0.30;
 
     public static BigDecimal calculateCost(LocalDateTime entry, LocalDateTime output) { // calcular custo do estacionamento
-        long minutes = entry.until(output, ChronoUnit.MINUTES);
+        long minutes = entry.until(output, ChronoUnit.MINUTES); // pegar a quantidade de tempo entre 2 objectos
         double total = 0.0;
 
-        if (minutes <= 15) {
-            total = FIRST_15_MINUTES;
+        if (minutes <= 10) {
+            total = FIRST_0_MINUTES;
         } else if (minutes <= 60) {
             total = FIRST_60_MINUTES;
         } else {
-            long additionalMinutes = minutes - 60;
-            Double totalParts = ((double) additionalMinutes / 15);
-            if (totalParts > totalParts.intValue()) { // 4.66 > 4
-                total += FIRST_60_MINUTES + (FIRST_60_MINUTES * (totalParts.intValue() + 1));
-            } else { // 4.0
-                total += FIRST_60_MINUTES + (FIRST_15_MINUTES * totalParts.intValue());
-            }
+            long additionalMinutes = (minutes - 60) / 60;
+            total = 300.00 + (100.00 * additionalMinutes);
         }
 
-        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN); // define a scala 2 caisa decimais
     }
 
 
